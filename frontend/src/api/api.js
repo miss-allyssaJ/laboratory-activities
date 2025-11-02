16d081739d31891eb6ae3977247c28a51693ec6c
icallyssajhonestanedo-spec <ic.allyssajhones.tanedo@cvsu.edu.ph>
2025-11-02 01:41:02 -0800

    Removed extra folders accidentally uploaded

--- frontend/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:3001';
-
-export const api = axios.create({
-  baseURL: API_BASE,
-  timeout: 5000,
-});
